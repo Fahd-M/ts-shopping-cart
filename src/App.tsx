b651cd8c3176443aeb2222f210be3e92ac976517
Fahd Munir <fahd.munir.89@gmail.com>
2023-01-12 12:44:48 -0500

    CartProvider and custom hook cart and components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Cart from "./components/Cart"
+import ProductList from "./components/ProductList"
+
+import { useState } from 'react'
 
 
 function App() {
+  const [viewCart, setViewCart] = useState<boolean>(false)
 
-  return (
-    <div className="App">
-     
-    </div>
+  // What is rendered is based on what viewcart is. if true render cart, if false render product list
+  const pageContent = viewCart ? <Cart /> : <ProductList />
+
+  const content = (
+    <>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {pageContent}
+      <Footer viewCart={viewCart} />
+    </>
   )
-}
 
+  return content
+    
+}
 export default App
+
+
+//NOTE: PRODUCTS ARRAY IN DATA FOLDER TO USE
+//TODO: create context for each view:(1) product display view (2) cart view
+// Cart context and products context
+
+//  TODO: HEADER AND FOOTER, AREA FOR PRODUCTS DISPLAY AND CART DISPLAY 
+
+// IMAGES FOLDER: for large projects where you pull from aws bucket 
+// TODO: dynamically pull images into project
+
+
+
+
+
